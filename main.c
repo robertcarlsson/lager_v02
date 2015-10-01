01e87b56eefc999a46c39b5a84adea3dbae760e1
Robert Carlsson <robert.carlsson.5461@student.uu.se>
2015-10-01 09:59:38 +0200

    Linux kan

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 /*
 
-Main loop with alternatives and 
+Main loop with alternatives and
 calls for options.c
 
 Will be using a global pointer as root node, so no need to send it to every function
@@ -8,36 +8,102 @@ try to make it as a constant if possible. So nothing can change it.
 
 
 */
+
 #include <stdio.h>
+#include <ctype.h>
+
 #include "main.h"
 #include "object.h"
 #include "tree.h"
 #include "undo.h"
 #include "stdscan.c"
 
+#define S_HEIGHT 40
+
+int load_menu();
+bool handle_menu_input(char);
+void clean_screen();
+void print_meny();
 
 int main() {
-  // I put the mainChoice to 1 instead of a do while loop.
-  char mainChoice = 1;
 
-  // While you dont Quit you keep going in the meny
-  while(mainChoice == 'Q' || mainChoice == 'q') {
+    load_menu();
+    return 0;
 
-    print_meny();
-    mainChoice = ask_for_char("Choice: ", "AaRrEePpUuLlQq");
+}
+
+// TODO Skapa en UI-modul eller liknande.
+
+int load_menu() {
+
+    bool exit = false;
+
+    clean_screen();
+
+    while(!exit) {
+
+
+        print_meny();
+
+        printf("What do you want to do?: ");
+        char input;
+        input = ask_for_char("Choice: ", "AaRrEePpUuLlQq");
+        exit = handle_menu_input(input);
+
 
     }
-  return 0;
+
+    printf("Exiting warehouse...");
+    return 0;
+
 }
 
-void print_meny() {
-  printf("\n\n Welcome to Warehouse 2\n"
-	 "[A]dd a good\n"
-	 "[R]emove a good\n"
-	 "[E]dit a good\n"
-	 "[P]rint a good\n"
-	 "[U]ndo a bad decision\n"
-	 "[L]ist everything\n"
-	 "[Q]uit this amazing program\n");
+bool handle_menu_input(char c) {
+
+    switch(tolower(c)) {
+
+        case 'a':
+        case 'r':
+        case 'e':
+        case 'i':
+        case 'w':
+        case 'u':
+            printf("Pressed %c.\n", c);
+            break;
+
+        case 'q':
+            return true;
+            break;
+
+        default:
+            printf("%c is not implemented yet.\n", c);
+            break;
+
+    }
+
+    return false;
+
 }
 
+void print_meny(){
+  printf("\nWarehouse program 2.0\n"
+  "===========================================\n"
+
+  "[a]dding a ware\n"
+  "[r]emoving a ware\n"
+  "[e]diting the information\n"
+  "[i]nformation about a ware\n"
+  "[w]ares in the database\n"
+  "[u]ndoing the last action\n"
+  "[q]uit\n");
+
+}
+
+void clean_screen() {
+
+  int i;
+
+  for ( i = 0; i < S_HEIGHT; i++ )
+    putchar ( '\n' );
+
+}
--- makefile
+++ makefile
@@ -1,8 +1,13 @@
 CC = gcc
 FLAGS = -Wall -std=c11 -ggdb
- 
+
 lager: main.c object.c options.c stdscan.c tree.c undo.c
-	$(CC) $(FLAGS) -o lager main.c
+	$(CC) $(FLAGS) -o lager.out main.c
 
 # %.o %.c %.h
-	$(CC) $(FLAGS) $^ -c	
+	$(CC) $(FLAGS) $^ -c
+
+clean:
+	find . -name "*.out" -type f -print -delete
+	find . -name "*.o" -type f -print -delete
+	find . -name "*.dSYM" -type d -print -exec rm -r {} +
